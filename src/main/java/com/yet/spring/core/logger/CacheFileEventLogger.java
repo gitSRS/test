d9897156f715d7b07e58df5a0151aecb2691365c
gitSRS <Streltsovrs@gmail.com>
2017-02-14 16:24:39 +0200

    video 7

---
+++ src/main/java/com/yet/spring/core/logger/CacheFileEventLogger.java
@@ -0,0 +1,41 @@
+package com.yet.spring.core.logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by RStreltsov on 13.02.2017.
+ */
+import com.yet.spring.core.Event;
+
+public class CacheFileEventLogger extends FileEventLogger {
+
+    private Integer cacheSize;
+    private List<Event> cache;
+
+    public CacheFileEventLogger(Integer cacheSize, String filename) {
+        super(filename);
+        this.cacheSize = cacheSize;
+        this.cache = new ArrayList<Event>(cacheSize);
+    }
+
+    public void destroy() {
+        if ( ! cache.isEmpty()) {
+            writeEventsFromCache();
+        }
+    }
+
+    @Override
+    public void logEvent(Event event) {
+        cache.add(event);
+
+        if (cache.size() == cacheSize) {
+            writeEventsFromCache();
+            cache.clear();
+        }
+    }
+
+    private void writeEventsFromCache() {
+        cache.stream().forEach(super::logEvent);
+    }
+}
